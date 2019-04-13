428e23f7f616ed7d830ed7c7394a9580253df836
Felix Stegmaier <stegmaier.felix@gmail.com>
2019-04-13 19:52:23 +0200

    Add babel settings to support asnyc

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,22 @@ import PromiseWorker from 'promise-worker';
 
 module.run();
 
-const worker = new Worker('worker.js');
-(new Promise((resolve, reject) => {
-    // must wait for the service worker to be alive before sending messages
-    // therefore we wait for it to send an message first
-    worker.onmessage = (event) => resolve(event.data);
-    worker.onerror = reject;
-})).then(() => {
-    console.log('initialized');
+async function loadWorker() {
+    const worker = new Worker('worker.js');
+    await new Promise((resolve, reject) => {
+        // must wait for the service worker to be alive before sending messages
+        // therefore we wait for it to send an message first
+        worker.onmessage = (event) => resolve(event.data);
+        worker.onerror = reject;
+    });
     const promiseWorker = new PromiseWorker(worker);
-    return promiseWorker.postMessage({ method: 'add', args: [1, 2]});
-}).then(res => {
-    console.log(res);
-})
+    return promiseWorker;
+}
+
+(async () => {
+ const worker = await loadWorker();
+ const res = await worker.postMessage({ method: 'add', args: [1, 4]});
+ console.log(res);
+})();
+
 
